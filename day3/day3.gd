f03906785a42953aee0b25ebfd9fd592bc8d8613
Kett Lovahr <catusfelony@gmail.com>
2023-12-03 03:58:33 -0300

    Day 3

---
+++ day3/day3.gd
@@ -0,0 +1,80 @@
+extends MainLoop
+
+func _initialize():
+	part_one()
+	part_two()
+
+func part_one():
+	var file = FileAccess.open("./input", FileAccess.READ)
+	var contents = file.get_as_text(true).trim_suffix("\n").split("\n")
+
+	var numbers: Array = []
+	var result: int = 0
+
+	for y in range(len(contents)):
+		var current_number = ""
+		for x in range(len(contents[y])):
+			if contents[y][x] in "0123456789":
+				current_number += contents[y][x]
+			if contents[y][x] not in "0123456789" or x == len(contents[y]) - 1:
+				if current_number != "":
+					numbers.append({
+							"number": int(current_number),
+							"line": y,
+							"row_start": x - len(current_number),
+							"row_end": x - 1,
+							"is_part_number": false,
+						})
+					current_number = ""
+	
+	for y in range(len(contents)):
+		for x in range(len(contents[y])):
+			if contents[y][x] not in "0123456789.":
+				for number in numbers:
+					if y in range(number["line"] - 1, number["line"] + 2):
+						if x in range(number["row_start"] - 1, number["row_end"] + 2):
+							number["is_part_number"] = true
+
+	for number in numbers:
+		if number["is_part_number"]:
+			result += number["number"]
+
+	print(result)
+
+func part_two():
+	var file = FileAccess.open("./input", FileAccess.READ)
+	var contents = file.get_as_text(true).trim_suffix("\n").split("\n")
+
+	var numbers: Array = []
+	var result: int = 0
+
+	for y in range(len(contents)):
+		var current_number = ""
+		for x in range(len(contents[y])):
+			if contents[y][x] in "0123456789":
+				current_number += contents[y][x]
+			if contents[y][x] not in "0123456789" or x == len(contents[y]) - 1:
+				if current_number != "":
+					numbers.append({
+							"number": int(current_number),
+							"line": y,
+							"row_start": x - len(current_number),
+							"row_end": x - 1,
+						})
+					current_number = ""
+	
+	for y in range(len(contents)):
+		for x in range(len(contents[y])):
+			if contents[y][x] == "*":
+				var neighbors: = []
+				for number in numbers:
+					if y in range(number["line"] - 1, number["line"] + 2):
+						if x in range(number["row_start"] - 1, number["row_end"] + 2):
+							neighbors.append(number["number"])
+				if len(neighbors) == 2:
+					result += neighbors[0] * neighbors[1]
+
+	print(result)
+
+func _process(_delta):
+	return 1
